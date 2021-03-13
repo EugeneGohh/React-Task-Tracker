784eb61d462f470ea742f44873c9ba4281d8e5af
Eugene Goh <gzkgt11@gmail.com>
2021-03-13 19:45:39 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
     getTasks();
   }, []);
 
-  // Fetch tasks
+  // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch("http://localhost:8000/tasks");
     const data = await res.json();
 
     return data;
@@ -29,7 +29,7 @@ function App() {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(`http://localhost:8000/tasks/${id}`);
     const data = await res.json();
 
     return data;
@@ -37,7 +37,7 @@ function App() {
 
   // Add Task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
+    const res = await fetch("http://localhost:8000/tasks", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
@@ -56,7 +56,7 @@ function App() {
 
   // Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
+    await fetch(`http://localhost:8000/tasks/${id}`, {
       method: "DELETE",
     });
 
@@ -68,7 +68,7 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`http://localhost:8000/tasks/${id}`, {
       method: "PUT",
       headers: {
         "Content-type": "application/json",
